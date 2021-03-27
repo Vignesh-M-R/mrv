97934800d2e6b3eda05f47b7769ffe1006a3e38f
Vignesh-M-R <003vigneshmr@gmail.com>
2021-03-27 16:59:57 +0530

    Resume Site 2.0

---
+++ sectionlink.js
@@ -0,0 +1,80 @@
+$("#knowmore").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $(".main").offset().top,
+    },
+    "slow"
+  );
+});
+
+$("#writeme").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $(".contact").offset().top,
+    },
+    "slow"
+  );
+});
+
+$("#fhome").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#home").offset().top,
+    },
+    "slow"
+  );
+});
+
+$("#fabout").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#about").offset().top,
+    },
+    "slow"
+  );
+});
+
+$("#fqualification").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#qualification").offset().top,
+    },
+    "slow"
+  );
+});
+
+$("#fskills").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#skills").offset().top,
+    },
+    "slow"
+  );
+});
+
+$("#fwhatido").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#whatido").offset().top,
+    },
+    "slow"
+  );
+});
+
+$("#fworks").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#works").offset().top,
+    },
+    "slow"
+  );
+});
+
+$("#fcontact").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#contact").offset().top,
+    },
+    "slow"
+  );
+});
---
+++ tooltip.js
@@ -0,0 +1,104 @@
+var spanText1 = document.getElementById("tt-html");
+      var spanText2 = document.getElementById("tt-css");
+      var spanText3 = document.getElementById("tt-js");
+      var spanText4 = document.getElementById("tt-react");
+      var spanText5 = document.getElementById("tt-ang");
+      var spanText6 = document.getElementById("tt-bts");
+      var spanText7 = document.getElementById("tt-sass");
+      var spanText8 = document.getElementById("tt-php");
+      var spanText9 = document.getElementById("tt-node");
+      var spanText10 = document.getElementById("tt-modb");
+      var spanText11 = document.getElementById("tt-figma");
+      var spanText12 = document.getElementById("tt-xd");
+      var spanText13 = document.getElementById("tt-db");
+      var spanText14 = document.getElementById("tt-native");
+      var spanText15 = document.getElementById("tt-ps");
+      var spanText16 = document.getElementById("tt-c");
+      var spanText17 = document.getElementById("tt-cpp");
+      var spanText18 = document.getElementById("tt-java");
+      var spanText19 = document.getElementById("tt-py");
+      var spanText20 = document.getElementById("tt-mysql");
+      var spanText21 = document.getElementById("tt-git");
+      var spanText22 = document.getElementById("tt-fbase");
+      var spanText23 = document.getElementById("tt-wrdprs");
+      var spanText24 = document.getElementById("tt-jquery");
+      var spanText25 = document.getElementById("tt-slsfrc");
+
+      window.onmousemove = function (e) {
+        var x = e.clientX,
+          y = e.clientY;
+        spanText1.style.top = y + 20 + "px";
+        spanText1.style.left = x + 20 + "px";
+
+        spanText2.style.top = y + 20 + "px";
+        spanText2.style.left = x + 20 + "px";
+
+        spanText3.style.top = y + 20 + "px";
+        spanText3.style.left = x + 20 + "px";
+
+        spanText4.style.top = y + 20 + "px";
+        spanText4.style.left = x + 20 + "px";
+
+        spanText5.style.top = y + 20 + "px";
+        spanText5.style.left = x + 20 + "px";
+
+        spanText6.style.top = y + 20 + "px";
+        spanText6.style.left = x + 20 + "px";
+
+        spanText7.style.top = y + 20 + "px";
+        spanText7.style.left = x + 20 + "px";
+
+        spanText8.style.top = y + 20 + "px";
+        spanText8.style.left = x + 20 + "px";
+
+        spanText9.style.top = y + 20 + "px";
+        spanText9.style.left = x + 20 + "px";
+
+        spanText10.style.top = y + 20 + "px";
+        spanText10.style.left = x + 20 + "px";
+
+        spanText11.style.top = y + 20 + "px";
+        spanText11.style.left = x + 20 + "px";
+
+        spanText12.style.top = y + 20 + "px";
+        spanText12.style.left = x + 20 + "px";
+
+        spanText13.style.top = y + 20 + "px";
+        spanText13.style.left = x + 20 + "px";
+
+        spanText14.style.top = y + 20 + "px";
+        spanText14.style.left = x + 20 + "px";
+
+        spanText15.style.top = y + 20 + "px";
+        spanText15.style.left = x + 20 + "px";
+
+        spanText16.style.top = y + 20 + "px";
+        spanText16.style.left = x + 20 + "px";
+
+        spanText17.style.top = y + 20 + "px";
+        spanText17.style.left = x + 20 + "px";
+
+        spanText18.style.top = y + 20 + "px";
+        spanText18.style.left = x + 20 + "px";
+
+        spanText19.style.top = y + 20 + "px";
+        spanText19.style.left = x + 20 + "px";
+
+        spanText20.style.top = y + 20 + "px";
+        spanText20.style.left = x + 20 + "px";
+
+        spanText21.style.top = y + 20 + "px";
+        spanText21.style.left = x + 20 + "px";
+
+        spanText22.style.top = y + 20 + "px";
+        spanText22.style.left = x + 20 + "px";
+
+        spanText23.style.top = y + 20 + "px";
+        spanText23.style.left = x + 20 + "px";
+
+        spanText24.style.top = y + 20 + "px";
+        spanText24.style.left = x + 20 + "px";
+
+        spanText25.style.top = y + 20 + "px";
+        spanText25.style.left = x + 20 + "px";
+      };
